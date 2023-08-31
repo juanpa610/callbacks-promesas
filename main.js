72a32dd6013f8b8f2bc22a1b3769ceb14b90721b
juanpa610 <jaramilloorregojuanpablo@gmail.com>
2023-08-31 16:34:35 -0500

    inicio proyecto

---
+++ main.js
@@ -0,0 +1,16 @@
+import './style.css'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="./javascript.svg" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>   
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
