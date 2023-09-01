13419f3770c286f00aa3904e85d4394004b09cb9
juanpa610 <jaramilloorregojuanpablo@gmail.com>
2023-09-01 10:10:24 -0500

    manejo de errores en callbacks

--- src/concepts/02-callbcks.js
+++ src/concepts/02-callbcks.js
@@ -8,8 +8,14 @@ import { heroes } from "../data/heroes";
  * @param {HTMLDivElement} element 
  */
 export const callbacksComponent = ( element ) => {
-                                      // esta función seria el callback            
-    findHero('5d86371f2343e37870b91ef1', (hero) => element.innerHTML =  hero?.name);
+                                      // esta función seria el callback  hero?.name || 'No hay Heroe'         
+    findHero('5d86371f2343e37870b91ef1', (err, hero) => {
+        if (err) {
+            element.innerHTML = err;
+            return;
+        }
+        element.innerHTML =  hero.name;
+    });
 
 };
 
@@ -17,11 +23,17 @@ export const callbacksComponent = ( element ) => {
 /**
  * 
  * @param {string} id 
- * @param { (hero : Object) => void } callback 
+ * @param { (error : String |Null, hero : Object) => void } callback 
  */
 const findHero = ( id, callback  ) => {
     
     const hero =  heroes.find( hero => hero.id === id);
 
-    callback( hero );
+    if( !hero ){
+        debugger
+        callback( `Hero with id ${id} not found.`);
+        return;
+    }
+
+    callback( null,  hero );
 }
