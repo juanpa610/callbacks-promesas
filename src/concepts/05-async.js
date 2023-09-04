423480cb25c5c50377f212b4f19bcf76a6ed3b2a
juanpa610 <jaramilloorregojuanpablo@gmail.com>
2023-09-04 11:11:18 -0500

    entendimiento async en Javascript

--- src/concepts/05-async.js
+++ src/concepts/05-async.js
@@ -1,6 +1,9 @@
 // async transforma para este ejemplo la función finHero para que esta retorne no 
 // el name de un hero si  no que retorne una promesa que resuelve el name de un hero;
 
+// las funciones asíncronas no siguen el hilo principal de código,
+// es como si se ejecutaran a un destiempo del hilo principal 
+
 import { heroes } from "../data/heroes";
 
 /**
@@ -10,12 +13,19 @@ import { heroes } from "../data/heroes";
 export const asyncComponent = ( element ) => {
 
     const idHero1 = '5d86371f1efebc31def272e2';
-
-    findHero(idHero1).then((name) => {
-        element.innerHTML = name;
-    }).catch((err) => {
-        element.innerHTML = err;
-    });
+    console.log('Inicio del componente ');
+    
+    // código asíncrono
+    findHero(idHero1)
+        .then((name) => {
+            element.innerHTML = name;
+            console.log(name);
+        }).catch((err) => {
+            element.innerHTML = err;
+        });
+    // fin código asíncrono
+    
+    console.log('Fin del componente ');
 };
 
 /**
